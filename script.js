7428dc90c209ab00a2dff1a03348f3c0154c2340
Pratik Kesharwani <104644580+pratikkw@users.noreply.github.com>
2024-01-01 21:15:02 -0500

    Added Preloader and Overlay

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 "use strict";
 
+const body = document.body;
+const overlay = document.querySelector(".overlay");
+const preloader = document.querySelector(".preloader");
+const preloaderImg = document.querySelector(".preloader img");
 const playlistArea = document.querySelector(".playlist__box");
 const playlistBtn = document.getElementById("playlist");
 const playlistCloseBtn = document.querySelector(".close");
 
-playlistBtn.addEventListener("click", function () {
-  playlistArea.classList.add("playlist__box--active");
+window.addEventListener("load", function () {
+  preloaderImg.src = "";
+  preloaderImg.alt = "";
+  preloader.classList.add("preloader--deactivate");
 });
 
-playlistCloseBtn.addEventListener("click", function () {
-  playlistArea.classList.remove("playlist__box--active");
-});
+const openPlaylist = function () {
+  playlistArea.classList.toggle("playlist__box--active");
+  overlay.classList.toggle("overlay--active");
+  body.classList.toggle("body-scroll--lock");
+};
+
+playlistBtn.addEventListener("click", openPlaylist);
+playlistCloseBtn.addEventListener("click", openPlaylist);
+overlay.addEventListener("click", openPlaylist);
