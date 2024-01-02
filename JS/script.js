3e64fb4c8e5951cfdbfe7bb6758e770a51946f7b
Pratik Kesharwani <104644580+pratikkw@users.noreply.github.com>
2024-01-02 12:41:48 -0500

    Adding functionality

--- JS/script.js
+++ JS/script.js
@@ -6,9 +6,42 @@ const preloader = document.querySelector(".preloader");
 const preloaderImg = document.querySelector(".preloader img");
 const playlistArea = document.querySelector(".playlist__box");
 const songLists = document.querySelector(".lists");
-const playlistBtn = document.getElementById("playlist");
 const playlistCloseBtn = document.querySelector(".close");
 
+const audioBox = document.querySelector(".our__audio");
+
+// SELECT IMG, TITLE, ARTIEST
+const songImg = document.getElementById("profile__img");
+const title = document.querySelector(".primary--title");
+const artiest = document.querySelector(".artiest");
+const currentTime = document.querySelector(".current__time");
+const totalDuration = document.querySelector(".total__duration");
+
+// CONTROLs
+const play_pauseBtn = document.getElementById("start_stop");
+const play_pauseSpan = document.getElementById("play_btn");
+const skipNext = document.getElementById("forward");
+const skipPrev = document.getElementById("previous");
+const loopBtn = document.getElementById("loop");
+const playlistBtn = document.getElementById("playlist");
+
+let allSong;
+let currentSong = 0;
+let maxLeng;
+
+// --> SETUP SONG
+const setupSong = function (num) {
+  const ourSong = allSong[num];
+
+  songImg.src = ourSong.img;
+  title.textContent = ourSong.title;
+  artiest.textContent = ourSong.artiest;
+  audioBox.src = ourSong.audio;
+  console.log(audioBox.readyState);
+};
+// -------------------------------
+
+// --> SETUP LIST WITH SONGs
 const addSongsToList = function (s) {
   const elements = s
     .map((item, ind) => {
@@ -31,18 +64,21 @@ const addSongsToList = function (s) {
   </li>`;
     })
     .join("");
-
   songLists.innerHTML = elements;
 };
+// -------------------------------
 
 // --> GET SONGs
 const getSongs = async function () {
   const result = await fetch("../JSON/musics.json");
   const { music } = await result.json();
+
+  allSong = music;
+  maxLeng = music.length;
   addSongsToList(music);
+  setupSong(currentSong);
 };
 getSongs();
-
 // -------------------------------
 
 // --> PLAYLIST OPEN/CLOSE
@@ -50,6 +86,39 @@ const openPlaylist = function () {
   playlistArea.classList.toggle("playlist__box--active");
   overlay.classList.toggle("overlay--active");
   body.classList.toggle("body-scroll--lock");
+};
+// -------------------------------
+
+// --> PLAY/PAUSE LOGIC
+const play_puase_Logic = function (element) {
+  if (element.innerText === "play_arrow") {
+    element.innerText = "pause";
+    audioBox.play();
+  } else if (element.innerText === "pause") {
+    element.innerText = "play_arrow";
+    audioBox.pause();
+  }
+};
+// -------------------------------
+
+// --> SKIP
+const skip = function () {
+  setupSong(currentSong);
+  audioBox.play();
+  play_pauseSpan.innerText = "pause";
+};
+// -------------------------------
+
+// DURATION
+const durationLogic = function () {
+  const songDura = audioBox.duration;
+  console.log(audioBox, songDura);
+  const min = Math.trunc(songDura / 60);
+  const sec = Math.trunc(songDura % 60);
+
+  totalDuration.textContent = `${min > 10 ? min : "0" + min}: ${
+    sec > 10 ? sec : "0" + sec
+  }`;
 };
 // -------------------------------
 
@@ -62,3 +131,20 @@ window.addEventListener("load", function () {
 playlistBtn.addEventListener("click", openPlaylist);
 playlistCloseBtn.addEventListener("click", openPlaylist);
 overlay.addEventListener("click", openPlaylist);
+
+play_pauseBtn.addEventListener("click", function (e) {
+  play_puase_Logic(e.target);
+  durationLogic();
+});
+
+skipNext.addEventListener("click", function () {
+  currentSong++;
+  currentSong = currentSong === maxLeng ? 0 : currentSong;
+  skip();
+});
+
+skipPrev.addEventListener("click", function () {
+  currentSong--;
+  currentSong = currentSong < 0 ? maxLeng - 1 : currentSong;
+  skip();
+});
